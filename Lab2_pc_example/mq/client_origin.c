ce95493d370a899cb062fc16839452fca9032055
owl-d <limdoyou000921@gmail.com>
2022-03-12 16:42:55 +0900

    Lab2

---
+++ Lab2_pc_example/mq/client_origin.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <mqueue.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MSG_Q_NAME  "/MSG_Q"
+#define NO_MAX_MSG  10
+#define MAX_MSG     1024
+#define STOP_CMD    "exit"
+
+int main(int agrc, char *argv[]) {
+  mqd_t msg_q;
+  struct mq_attr attr;
+  int mq_len;
+  char message[MAX_MSG];
+
+  attr.mq_flags = 0;
+  attr.mq_maxmsg = NO_MAX_MSG;
+  attr.mq_msgsize = MAX_MSG;
+  attr.mq_curmsgs = 0;
+
+  msg_q = mq_open (MSG_Q_NAME,O_WRONLY,S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH, &attr);
+  if ( -1 == msg_q) {
+    perror("mq_open");
+    _exit(-1);
+  }
+
+  printf("Enter \"exit\"  to stop: \n");
+  do {
+    bzero(message, MAX_MSG);
+    printf("Send> ");
+    fgets(message,sizeof(message), stdin);
+    message[strlen(message)-1]='\0';
+    mq_len = strlen(message);
+    if ( -1 == mq_send(msg_q, message, mq_len, 0)) {
+      perror("mq_send");
+      mq_close(msg_q);
+      mq_unlink(MSG_Q_NAME);
+      _exit(-1);
+    }
+  } while (!(0 == strcmp(message, STOP_CMD)));
+
+  printf("mq_writer: Exit\n");
+  mq_close(msg_q);
+  mq_unlink(MSG_Q_NAME);
+  return 0;
+}
---
+++ Lab2_pc_example/semaphore/semaphore_ex2.c
@@ -0,0 +1,81 @@
+#include <unistd.h>     
+#include <sys/types.h>  
+#include <errno.h>      
+#include <stdio.h>      
+#include <stdlib.h>    
+#include <pthread.h>   
+#include <string.h>    
+#include <semaphore.h>
+
+sem_t binaySemphore;
+
+void* thread_1_function(void *ptr);
+void* thread_2_function(void *ptr);
+
+int main()
+{
+    int iRet;
+    pthread_t thread_1;
+    pthread_t thread_2;
+    unsigned char ucBuff[10];
+
+    /* second param = 0  - semaphore is local() */
+    /* second param = 1  - semaphore is global*/
+    /* third param  = n  - semaphore count value*/
+    sem_init(&binaySemphore, 0, 1);    
+
+    strcpy(ucBuff,"thread_1");
+    iRet=pthread_create(&thread_1, NULL,thread_1_function,(void *)ucBuff);
+    if(iRet == 0)
+    {
+        printf("pthread 1 created...\n");
+    }
+
+    sleep(1);
+    strcpy(ucBuff,"thread_2");
+    iRet=pthread_create(&thread_2, NULL,thread_2_function,(void *)ucBuff);
+    if(iRet == 0)
+    {
+        printf("pthread 2 created...\n");
+    }
+
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
+
+    sem_destroy(&binaySemphore); /* destroy semaphore */
+    exit(0);
+}
+
+
+/* prototype for thread routine */
+void* thread_1_function(void *ptr)
+{
+    unsigned char* ucBuffPtr,ucThreadBuff[10];
+    ucBuffPtr = (unsigned char *) ptr;
+    strcpy(ucThreadBuff,ucBuffPtr);
+    printf("thread_1_function entered\n");
+    
+    sem_wait(&binaySemphore);      /* down semaphore */
+    printf("Semaphore is with %s\n",ucThreadBuff);
+    sleep(5);
+    sem_post(&binaySemphore);       /* up semaphore */
+    sleep(1);
+    
+    pthread_exit(0); /* exit thread */
+}
+
+void* thread_2_function(void *ptr)
+{
+    unsigned char* ucBuffPtr,ucThreadBuff[10];
+    ucBuffPtr = (unsigned char *) ptr;
+    strcpy(ucThreadBuff,ucBuffPtr);
+    printf("thread_2_function entered\n");
+    
+    sem_wait(&binaySemphore);      /* down semaphore */
+    printf("Semaphore is with %s\n",ucThreadBuff);
+    sleep(1);
+    sem_post(&binaySemphore);       /* up semaphore */
+    sleep(1);
+    
+    pthread_exit(0); /* exit thread */
+}
